d57b895c7c9b823243c09ce4a9ae0860c2fed8af
kaualucio <kauadefreitas.s992@gmail.com>
2025-02-18 07:10:22 -0300

    add services texts

--- src/lib/services.tsx
+++ src/lib/services.tsx
@@ -6,19 +6,19 @@ export const services = [
   {
   icon: <IdvIcon className="size-12 text-[#2E2E2E] group-hover:text-[#6C6C6C] duration-200" />,
   title: "Identidade Visual",
-  description: "Lorem ipsum dolor sit amet consectetur. Integer in curabitur pretium egestas. Neque tempor neque platea pharetra elit tempor. Morbi magna nisi nullam consequat adipiscing",
+  description: "Cada marca que criamos é pensada com exclusividade, desde o conceito até a aplicação final. Nosso compromisso é com um processo seguro, estruturado e personalizado, garantindo que sua identidade seja transmitida de forma memorável.",
   link: "https://api.whatsapp.com/send?phone=5584996419255&text=Ol%C3%A1,%20gostaria%20de%20tirar%20algumas%20d%C3%BAvidas"
   },
   {
     icon:  <SiteIcon className="size-12 text-[#2E2E2E] group-hover:text-[#6C6C6C] duration-200" />,
     title: "Criação de Sites",
-    description: "Criamos sites incríveis, personalizados e estratégicos para destacar seu negócio na internet. Com design impecável, alta performance e experiência intuitiva, garantimos uma presença digital forte e profissional para atrair mais clientes e gerar resultados reais! ",
+    description: "Seu site é o cartão de visitas digital do seu negócio, e na Asterisco, criamos soluções que vão muito além do design bonito. Estruturamos cada página para oferecer navegação intuitiva, responsividade impecável e estratégias funcionais que impulsionam sua presença digital e conversões. ",
     link: "https://api.whatsapp.com/send?phone=5584996419255&text=Ol%C3%A1,%20gostaria%20de%20tirar%20algumas%20d%C3%BAvidas"
   },
   {
     icon:  <VideoIcon className="size-12 text-[#2E2E2E] group-hover:text-[#6C6C6C] duration-200" />,
     title: "Edição de Videos",
-    description: "Lorem ipsum dolor sit amet consectetur. Integer in curabitur pretium egestas. Neque tempor neque platea pharetra elit tempor. Morbi magna nisi nullam consequat adipiscing",
+    description: "Transformamos conteúdos em experiências visuais marcantes. Cada edição é feita com precisão, criatividade e intencionalidade para engajar, emocionar e contar sua história da melhor forma. Nosso compromisso é garantir fluidez, ritmo e uma comunicação visual impactante que representa sua marca com autenticidade.",
     link: "https://api.whatsapp.com/send?phone=5584996419255&text=Ol%C3%A1,%20gostaria%20de%20tirar%20algumas%20d%C3%BAvidas"
   },  
 ]
