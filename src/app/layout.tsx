69650d2340b6df73e21a11129620ec30d1b4fca8
kaualucio <kauadefreitas.s992@gmail.com>
2025-02-26 07:06:58 -0300

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Sora, Schoolbell, Baloo_2 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
@@ -47,6 +48,18 @@ export default function RootLayout({
       <body
         className={`${sora.variable} ${schoolbell.variable} ${baloo_2.variable} antialiased bg-[#0c0d0c]`}
       >
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-7CFHBDMMV6`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7CFHBDMMV6');
+          `}
+        </Script>
         <Header/>
         {children}
         <Footer />
